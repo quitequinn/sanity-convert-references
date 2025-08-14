bc5de3b58dce93622b20662cb072ac4620250e59
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 11:13:28 -0700

    Update index to export ConvertReferences component

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Sanity Convert References - Main Export
+ * Convert between strong and weak references in Sanity documents
+ */
+
+export { default as ConvertReferences } from './ConvertReferences'
+export { default } from './ConvertReferences'
+
+// Export types if needed
+export type {
+  ConvertReferencesProps,
+  ConversionResult
+} from './ConvertReferences'
